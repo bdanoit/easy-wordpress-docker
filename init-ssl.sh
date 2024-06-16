7a5d7945bcad4df628cd496194bf57908d55c5e2
baleze <baleze@danoit.com>
2024-06-16 16:41:30 -0700

    modified init-ssl to challenge using nginx.challenge.conf

--- init-ssl.sh
+++ init-ssl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#cat .env | grep HOSTNAME= | tr -d HOSTNAME= | \
-docker compose up -d webserver && \
-docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run
+docker compose kill webserver
+docker compose run -d -e NGINX_CONFIG_FILE=nginx.challenge.conf webserver
+docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/
