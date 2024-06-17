28655e8f3f327d03c3970ead0610acd6c9afb1a5
bdanoit <bdanoit@gmail.com>
2024-06-17 10:58:17 -0700

    -e flag did not work as intended, switched to export

--- init-ssl.sh
+++ init-ssl.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-docker compose kill webserver
-docker compose run -d -P -e NGINX_CONFIG_FILE=nginx.challenge.conf webserver
+#use the nginx.challenge template to prevent missing certificate error
+export NGINX_CONFIG_FILE=nginx.challenge.conf && \
+docker compose up -d webserver
 docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/
+#reset to default template
+export NGINX_CONFIG_FILE=nginx.conf && \
+docker compose up -d webserver
